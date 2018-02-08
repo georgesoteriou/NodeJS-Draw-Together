d472bd670454032e3fa3b8c8715102023441dbc3
gs2617 <gs2617@ic.ac.uk>
2018-02-08 14:32:42 +0000

    fixed size

--- public/scetch.js
+++ public/scetch.js
@@ -2,33 +2,31 @@ var socket = io();
 var my_col
 
 function setup(){
-	createCanvas(800,800);
+	createCanvas(windowWidth-25,windowHeight-25);
 	background(61);
-    my_col = {R: random(255), G: random(255), B:random(255)}
-    button = createButton('clear');
-    button.mousePressed(clr_emit);
-    noStroke();
+    	my_col = {R: random(255), G: random(255), B:random(255)}
+    	button = createButton('Clear');
+    	button.mousePressed(clr_emit);
+    	noStroke();
 }
 
-function draw(){
-	if (mouseIsPressed) {
-		fill(my_col.R,my_col.G,my_col.B);
-		ellipse(mouseX,mouseY,10,10);
-		socket.emit('ellipse',{X:mouseX,Y:mouseY,col: my_col});
-	}
+function mouseDragged() {
+	var xPos = mouseX/width;
+	var yPos = mouseY/height;
+	fill(my_col.R,my_col.G,my_col.B);
+	ellipse(mouseX,mouseY,10,10);
+	socket.emit('ellipse',{X:xPos,Y:yPos,col: my_col});
 }
 
 function clr_emit(){
-    socket.emit('clear');
+    	socket.emit('clear');
 }
 
-function clr_on(){
-    background(61);
-}
-
-socket.on('clr',clr_on);
+socket.on('clr',function(){
+    	background(61);
+});
 
 socket.on('other_ellipse', function (data) {
 	fill(data.col.R,data.col.G,data.col.B);
-	ellipse(data.X,data.Y,10,10);
+	ellipse(data.X*width,data.Y*height,10,10);
 });
