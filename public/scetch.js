4099ae03eb2276a323977b4a3c1244a558f66370
gs2617 <gs2617@ic.ac.uk>
2018-02-08 14:48:19 +0000

    fixed object size

--- public/scetch.js
+++ public/scetch.js
@@ -1,6 +1,6 @@
 var socket = io();
 var my_col
-var size = 18;
+var Esize = 18;
 
 function setup(){
 	createCanvas(windowWidth-25,windowHeight-25);
@@ -15,7 +15,7 @@ function mouseDragged() {
 	var xPos = mouseX/width;
 	var yPos = mouseY/height;
 	fill(my_col.R,my_col.G,my_col.B);
-	ellipse(mouseX,mouseY,size,size);
+	ellipse(mouseX,mouseY,Esize,Esize);
 	socket.emit('ellipse',{X:xPos,Y:yPos,col: my_col});
 }
 
@@ -29,5 +29,5 @@ socket.on('clr',function(){
 
 socket.on('other_ellipse', function (data) {
 	fill(data.col.R,data.col.G,data.col.B);
-	ellipse(data.X*width,data.Y*height,size,size);
+	ellipse(data.X*width,data.Y*height,Esize,Esize);
 });
