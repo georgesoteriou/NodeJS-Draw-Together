c253d9e523e869455f56e325eb7f28aa09af155d
gs2617 <gs2617@ic.ac.uk>
2018-03-16 16:41:32 +0000

    upgraded interface

--- public/scetch.js
+++ public/scetch.js
@@ -1,27 +1,30 @@
 var socket = io();
-var g,n,my_col,myp5;
-var w = window.innerWidth-25;
-var h = window.innerHeight-25;
+$(function() {
+    window.location.hash = "";
+    socket.emit('showAllGames');
+    var g,n,my_col,myp5;
+    var w = window.innerWidth;
+    var h = window.innerHeight;
 
-var s = function( p ) { 
-    var clear_b, toggle_b,canvas;
-    var Esize = 18;
+    var s = function( p ) {
+        var clear_b, toggle_b,canvas;
+        var Esize = 18;
 
-    p.setup = function() {
-        my_col = {R: p.random(255), G: p.random(255), B:p.random(255)};
-        socket.emit('start', {name: n, col:my_col});
-    }
+        p.setup = function() {
+            my_col = {R: p.random(255), G: p.random(255), B:p.random(255)};
+            socket.emit('start', {name: n, col:my_col});
+        }
 
-    socket.on('join',function(data){
-        canvas = p.createCanvas(data.width,data.height);
-        p.background(61);
-        p.strokeWeight(5);
-    });
+        socket.on('join',function(data){
+            canvas = p.createCanvas(data.width,data.height);
+            p.background(61);
+            p.strokeWeight(5);
+        });
 
-    var singleTouch = true;
-    var draw_t = true;
+        var singleTouch = true;
+        var draw_t = true;
 
-    $(function() {
+
         $('body').on('touchstart touchmove', function (e) {
             console.log(e.touches.length);
             if (e.touches.length > 1){
@@ -31,102 +34,129 @@ var s = function( p ) {
                 e.preventDefault();
             }
         });
+
+        p.touchMoved = function(){
+            if(singleTouch){
+                if(draw_t){
+                    var mx = p.mouseX  / w;
+                    var my = p.mouseY  / h;
+                    var px = p.pmouseX / w;
+                    var py = p.pmouseY / h;
+                    var pos = [mx,my,px,py];
+                    p.stroke(my_col.R,my_col.G,my_col.B);
+                    p.line(p.mouseX,p.mouseY,p.pmouseX,p.pmouseY);
+                    socket.emit('draw',{type:'line',pos:pos,col: my_col,game:g});
+                }else{
+                    var mx = p.mouseX  / w;
+                    var my = p.mouseY  / h;
+                    var pos = [mx,my];
+                    p.noStroke();
+                    p.fill(61);
+                    p.ellipse(p.mouseX,p.mouseY,50,50);
+                    socket.emit('draw',{type:'erase',pos:pos,col: my_col,game:g});
+                }
+            }
+        }
+
+        function clr_emit(){
+            socket.emit('clear',{game:g});
+        }
+
+        function toggle(){
+            if(draw_t){
+                draw_t = false;
+                toggle_b.html('Draw');
+            }else{
+                draw_t = true;
+                toggle_b.html('Eraser');
+            }
+        }
+
+        socket.on('other_draw', function (data) {
+            if(data.game == g){
+                if(data.type === 'line'){
+                    p.stroke(data.col.R,data.col.G,data.col.B);
+                    p.line(data.pos[0]*w,data.pos[1]*h,data.pos[2]*w,data.pos[3]*h);
+                }else{
+                    p.noStroke();
+                    p.fill(61);
+                    p.ellipse(data.pos[0]*w,data.pos[1]*h,50,50);
+                }
+            }
+        });
+
+        socket.on('clr',function(data){
+            if(data.game == g){
+                p.background(61);
+            }
+        });
+
+        p.makebuttons = function(){
+            clear_b = p.createButton('Clear');
+            toggle_b = p.createButton('Eraser');
+            clear_b.parent('header');
+            toggle_b.parent('header');
+            clear_b.mousePressed(clr_emit);
+            toggle_b.mousePressed(toggle);
+        }
+    };
+
+    socket.on('userUpdate',function(data){
+        if(data.game == g){
+            $('#users').html('Online Users');
+            data.users.forEach(user => $('#users').append("<li style='color: rgb("
+                +Math.floor(user.col.R)+", "
+                +Math.floor(user.col.G)+", "
+                +Math.floor(user.col.B)+")'>"+user.name+"</li>"));
+        }
     });
 
-    p.touchMoved = function(){
-        if(singleTouch){
-        if(draw_t){
-            var mx = p.mouseX  / w;
-            var my = p.mouseY  / h;
-            var px = p.pmouseX / w;
-            var py = p.pmouseY / h;
-            var pos = [mx,my,px,py];
-            p.stroke(my_col.R,my_col.G,my_col.B);
-            p.line(p.mouseX,p.mouseY,p.pmouseX,p.pmouseY);
-            socket.emit('draw',{type:'line',pos:pos,col: my_col,game:g});
+    socket.on('allGames',function(data){
+        $('#openGames').html('');
+        data.forEach(g => {
+            $('#openGames').append("<option value ='"+g.game+"' label = 'Users: "+g.users.length+", Size: "+g.width+"x"+g.height+"'>");
+        });
+    });
+
+
+    $('.play' ).on( 'click', function() {
+        g = $('#game').val();
+        if(g != ""){
+            window.location.hash = g;
+        }
+    });
+
+    $(window).on('hashchange', function(){
+        socket.emit('discon');
+        if(window.location.hash == ""){
+            $('canvas').hide();
+            $('#header').hide();
+            $('#users').hide();
+            $('.title-screen').show();
+            socket.emit('showAllGames');
+            $('#game').val('');
         }else{
-            var mx = p.mouseX  / w;
-            var my = p.mouseY  / h;
-            var pos = [mx,my];
-            p.noStroke();
-            p.fill(61);
-            p.ellipse(p.mouseX,p.mouseY,50,50);
-            socket.emit('draw',{type:'erase',pos:pos,col: my_col,game:g});
-        }
-        }
-    }
-
-    function clr_emit(){
-        socket.emit('clear',{game:g});
-    }
-
-    function toggle(){
-        if(draw_t){
-            draw_t = false;
-            toggle_b.html('Draw');
-        }else{
-            draw_t = true;
-            toggle_b.html('Eraser');
-        }
-    }
-
-    socket.on('other_draw', function (data) {
-        if(data.game == g){
-            if(data.type === 'line'){
-                p.stroke(data.col.R,data.col.G,data.col.B);
-                p.line(data.pos[0]*w,data.pos[1]*h,data.pos[2]*w,data.pos[3]*h);
-            }else{
-                p.noStroke();
-                p.fill(61);
-                p.ellipse(data.pos[0]*w,data.pos[1]*h,50,50);
+            g = window.location.hash.substring(1);
+            n = $('#name').val();
+            if(!n){
+                n = "Anon";
+            }
+            if(g != ""){
+                socket.emit('left',{game:g});
+                socket.emit('init',{game:g,w:w,h:h});
+                if(myp5){
+                    myp5.setup();
+                    $('canvas').show();
+                    $('#header').show();
+                    $('#users').show(); 
+                }else{
+                    myp5 = new p5(s);
+                    myp5.makebuttons();
+                }
+                $('.title-screen').hide();
             }
         }
     });
-
-    socket.on('clr',function(data){
-        if(data.game == g){
-            p.background(61);
-        }
-    });
-
-    p.makebuttons = function(){
-        clear_b = p.createButton('Clear');
-        toggle_b = p.createButton('Eraser');
-        clear_b.parent('header');
-        toggle_b.parent('header');
-        clear_b.mousePressed(clr_emit);
-        toggle_b.mousePressed(toggle);
-    }
-};
-
-socket.on('userUpdate',function(data){
-    if(data.game == g){
-        $('#users').html('Users:');
-        data.users.forEach(user => $('#users').append("<li style='color: rgb("
-            +Math.floor(user.col.R)+", "
-            +Math.floor(user.col.G)+", "
-            +Math.floor(user.col.B)+")'>"+user.name+"</li>"));
-    }
 });
-
-function play() {
-    g = $('#game').val();
-    n = $('#name').val();
-    if(!n){
-        n = "Anon";
-    }
-    if(g != ""){
-        socket.emit('left',{game:g});
-        socket.emit('init',{game:g,w:w,h:h});
-        if(myp5){
-            myp5.setup();
-        }else{
-            myp5 = new p5(s);
-            myp5.makebuttons();
-        }
-    }
-}
-
-
 
 
--- server.js
+++ server.js
@@ -3,31 +3,44 @@ var express = require('express');
 var port = process.env.PORT || 5050;
 
 var server = express()
-    .use(express.static(__dirname + '/public'))
-    .use(express.static(__dirname))
-    .set('views', __dirname + '/public')
-    .engine('.html', require('ejs').renderFile)
-    .get('/', function(req, res) {
-        res.render('index.html');
-    })
-    .listen(port, function() { console.log("Listening on port " + port); });
+.use(express.static(__dirname + '/public'))
+.use(express.static(__dirname))
+.set('views', __dirname + '/public')
+.engine('.html', require('ejs').renderFile)
+.get('/', function(req, res) {
+    res.render('index.html');
+})
+.listen(port, function() { console.log("Listening on port " + port); });
 
 var io = require('socket.io')(server);
-var all = [];
+'use strict';
+
+const fs = require('fs');
+
+var json;
+
+fs.readFile('data.json', (err, data) => {
+    json = JSON.parse(data);
+});
 
 io.on('connection', function(socket) {
     var g;
     var user;
-	console.log('connection');
+    console.log('connection');
     socket.on('init', function(data){
-        maybeg = all.filter(e => e.game == data.game);
+        maybeg = json.all.filter(e => e.game == data.game);
         if (maybeg.length == 0){
             g = {game:data.game, draw: [], width:data.w, height:data.h, users: []};
             console.log(g.width,g.height);
-            all.push(g);
+            json.all.push(g);
         }else{
             g = maybeg[0];
         }
+        fs.writeFileSync('data.json', JSON.stringify(json));
+    });
+
+    socket.on('showAllGames',function(){
+        socket.emit('allGames', json.all);
     });
 
     socket.on('start', function(data){
@@ -36,8 +49,9 @@ io.on('connection', function(socket) {
         io.emit('userUpdate',{game:g.game, users: g.users});
         socket.emit('join', {width:g.width,height:g.height});
         g.draw.forEach(function(e){
-    	    socket.emit('other_draw',e);
-        });
+           socket.emit('other_draw',e);
+       });
+        fs.writeFileSync('data.json', JSON.stringify(json));
     });
 
     socket.on('left',function(data){
@@ -45,31 +59,51 @@ io.on('connection', function(socket) {
             g.users = g.users.filter(e => e != user);
             io.emit('userUpdate',{game:g.game, users: g.users});
         }
+        fs.writeFileSync('data.json', JSON.stringify(json));
     });
 
     socket.on('draw', function(data) {
-        if(all.some(e => e.game == data.game)){
+        if(json.all.some(e => e.game == data.game)){
             g.draw.push(data);
             socket.broadcast.emit('other_draw', data);
         }
+        fs.writeFileSync('data.json', JSON.stringify(json));
     });
 
     socket.on('disconnect', function(){
-		console.log('user disconnected');
+        console.log('user disconnected');
         if(g){
             g.users = g.users.filter(e => e != user);
             io.emit('userUpdate',{game:g.game, users: g.users});
+            if(g.users.length == 0 && g.draw.length == 0){
+                json.all = json.all.filter(e => e.game != g.game);
+            }
         }
-	});
+        fs.writeFileSync('data.json', JSON.stringify(json));
+    });
 
-	socket.on('clear',function(data){
-        if(all.some(e => e.game == data.game)){
+    socket.on('discon', function(){
+        console.log('user changed game');
+        if(g){
+            g.users = g.users.filter(e => e != user);
+            io.emit('userUpdate',{game:g.game, users: g.users});
+            if(g.users.length == 0 && g.draw.length == 0){
+                json.all = json.all.filter(e => e.game != g.game);
+            }
+        }
+        fs.writeFileSync('data.json', JSON.stringify(json));
+    });
+
+    socket.on('clear',function(data){
+        if(json.all.some(e => e.game == data.game)){
         	g.draw = [];
         	io.emit('clr',data);
         }
-	});
+        fs.writeFileSync('data.json', JSON.stringify(json));
+    });
 
     socket.on('clearall',function(){
-        all = [];
+        json.all = [];
+        fs.writeFileSync('data.json', JSON.stringify(json));
     });
 });
